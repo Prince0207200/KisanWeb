4951e2b1964cebc949f4fca78bfc1bcadc924be9
Prince <princegupta1455@gmail.com>
2025-02-24 00:58:27 +0530

    commiting all code

---
+++ newProject/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './index.css'
+function App() {
+  return (
+    <div className='outer-div'>
+      <h1>Sample Picture</h1>
+
+
+
+      <div className='Outer1'>
+        <div class="picture1 inner-1"></div>
+        <div class="picture1 inner-1"></div>
+        <div class="picture1 inner-1"></div>
+      </div>
+      <div className='Outer2'>
+        <div class="picture1 inner-1"></div>
+        <div class="picture1 inner-1"></div>
+        <div class="picture1 inner-1"></div>
+      </div>
+      <div className='Outer3'>
+        <div class="picture1 inner-1"></div>
+        <div class="picture1 inner-1"></div>
+
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
